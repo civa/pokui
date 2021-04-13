d26e662e3903881f2d4d621ab2cc6be78fd4e71a
mobin-ux <mobin.gh505@gmail.com>
2021-04-13 23:57:29 +0000

    update logo

--- src/widgets/Menu/icons/Logo.tsx
+++ src/widgets/Menu/icons/Logo.tsx
@@ -9,13 +9,7 @@ interface LogoProps extends SvgProps {
 const Logo: React.FC<LogoProps> = ({ isDark, ...props }) => {
   const textColor = isDark ? "#FFFFFF" : "#000000";
   return (
-    <Svg viewBox="0 0 205 26" {...props}>
-      <image
-        width="205"
-        height="26"
-        href={isDark ? "/images/egg/LogoTextNewDark.png" : "/images/egg/LogoTextNewWhite.png"}
-      />
-    </Svg>
+    <img src="/images/dry/DRYCAKE-logo-new.png" width="205" {...props} />
   );
 };
 
