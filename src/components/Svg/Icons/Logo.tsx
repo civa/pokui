095238b7d7d7ee0af15a17246f500411f87b9311
mobin-ux <mobin.gh505@gmail.com>
2021-04-14 00:10:40 +0000

    fix bug

--- src/components/Svg/Icons/Logo.tsx
+++ src/components/Svg/Icons/Logo.tsx
@@ -4,9 +4,7 @@ import { SvgProps } from "../types";
 
 const Icon: React.FC<SvgProps> = (props) => {
   return (
-    <Svg viewBox="0, 90, 375, 180" {...props}>
      <img src="/images/dry/DRYCAKE-logo-new.png" width="205" {...props} />
-    </Svg>
   );
 };
 
