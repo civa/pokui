81796727207f1af603fa57f2d201468c8692a9ff
Civa <civa@cryptodeploy.io>
2021-04-17 11:26:51 +0000

    update

--- src/theme/colors.ts
+++ src/theme/colors.ts
@@ -41,7 +41,7 @@ export const darkColors: Colors = {
   backgroundDisabled: "#3c3742",
   contrast: "#FFFFFF",
   invertedContrast: "#191326",
-  input: "#ae8761",
+  input: "#b89777",
   primaryDark: "#0098A1",
   tertiary: "#a27a53",
   text: "#EAE2FC",
--- src/widgets/Menu/icons/Logo.tsx
+++ src/widgets/Menu/icons/Logo.tsx
@@ -9,9 +9,9 @@ interface LogoProps extends SvgProps {
 const Logo: React.FC<LogoProps> = ({ isDark, ...props }) => {
   const textColor = isDark ? "#FFFFFF" : "#000000";
   return (
-    <Svg viewBox="0 0 500 500" {...props}>
-      <image width="32" height="32" href="/images/dry/IMG-20210318-213156-858-removebg-preview.png" />
-    </Svg>
+  
+      <img width="32" height="32" src="/images/dry/IMG-20210318-213156-858-removebg-preview.png" {...props}/>
+    
   );
 };
 
