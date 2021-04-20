2ab6de5e7296252c69a1bbeed55729cac1460460
mobin-ux <mobin.gh505@gmail.com>
2021-04-20 20:02:06 +0000

    update

--- src/components/Svg/Icons/Logo.tsx
+++ src/components/Svg/Icons/Logo.tsx
@@ -3,7 +3,7 @@ import Svg from "../Svg";
 import { SvgProps } from "../types";
 
 const Icon: React.FC<SvgProps> = (props) => {
-  return <img width="32" height="32" src="/images/dry/logo.png" {...props} />;
+  return <img width="32" height="32" src="/images/24k-test/logo.svg" {...props} />;
 };
 
 export default Icon;
