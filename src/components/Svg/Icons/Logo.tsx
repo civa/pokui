cc6bc1b434c101f070c87476582613eb2b9ad37c
Civa <44102586+civa@users.noreply.github.com>
2021-04-20 22:13:41 +0100

    Update Logo.tsx

--- src/components/Svg/Icons/Logo.tsx
+++ src/components/Svg/Icons/Logo.tsx
@@ -3,7 +3,7 @@ import Svg from "../Svg";
 import { SvgProps } from "../types";
 
 const Icon: React.FC<SvgProps> = (props) => {
-  return <img width="32" height="32" src="/images/24k-test/logo.svg" {...props} />;
+  return <img width="32" height="32" src="/images/dry/logo.png" {...props} />;
 };
 
 export default Icon;
