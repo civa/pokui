51cbaeb734057faee2a6bbf2e217480ea559a2ec
mobin-ux <mobin.gh505@gmail.com>
2021-04-21 11:30:49 +0000

    update

--- dist/widgets/Menu/icons/index.d.ts
+++ dist/widgets/Menu/icons/index.d.ts
@@ -1,3 +1,4 @@
+export { default as AuditIcon } from "./Audit";
 export { default as FarmIcon } from "./Farm";
 export { default as GroupsIcon } from "./Groups";
 export { default as HamburgerIcon } from "./Hamburger";
@@ -17,7 +18,6 @@ export { default as TicketIcon } from "./Ticket";
 export { default as TradeIcon } from "./Trade";
 export { default as TwitterIcon } from "./Twitter";
 export { default as RedditIcon } from "./Reddit";
-export { default as AuditIcon } from "./Audit";
 export { default as GooseIcon } from "./Goose";
 export { default as HandshakeIcon } from "./Handshake";
 export { default as LayerIcon } from "./Layer";
